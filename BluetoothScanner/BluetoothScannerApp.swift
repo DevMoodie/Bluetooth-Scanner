7909714ee2678e9297c86709c83f284ea4c6cfc1
Moodie <admin@deenoverdunya.net>
2022-11-08 15:01:19 -0500

    Initial Commit

---
+++ BluetoothScanner/BluetoothScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothScannerApp.swift
+//  BluetoothScanner
+//
+//  Created by Mahmoud Youssef on 2022-11-08.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
